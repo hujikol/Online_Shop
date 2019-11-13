41a2fb17d54b46896a270a11c5c58af4df0ac2cf
Nicholas Nanda <nikolasnanda@gmail.com>
2019-11-13 20:50:51 +0700

    perbaruan tampilan shop
    tambah add to cart, qty, product id
    hover image buat liat harga *blm jadi

--- header.php
+++ header.php
@@ -1,27 +1,33 @@
-<?php 
+<?php
 session_start();
 include 'koneksi.php';
 ?>
 
 <html>
+
 <head>
     <link rel="stylesheet" href="css/index.css">
     <title>illegal</title>
 </head>
+
 <body>
-    <div class="logo">
-        <a href="shop.php" title="illegal">i l l e g a l</a>
+    <div class="header">
+        <div class="logo">
+            <a href="shop.php" title="illegal">i l l e g a l</a>
+        </div>
+        <?php if (isset($_SESSION['login'])) : ?>
+            
+                <ul class="navbar">
+                    <li><a href="shop.php" title="Shop">Shop</a></li>
+                    <li><a href="cart.php" title="Cart">Cart</a></li>
+                    <li><a href="profile.php" title="Account">Account</a></li>
+                    <li><a href="logout.php" title="Logout">Logout</a></li>
+                </ul>
+            
+        <?php else : ?>
+            <ul class="navbar">
+                <li><a href="shop.php" title="Shop">Shop</a></li>
+                <li><a href="login.php" title="Login">Login</a></li>
+            </ul>
     </div>
-    <?php if(isset($_SESSION['login'])) : ?>
-        <ul class="navbar">
-            <li><a href="shop.php" title="Shop">Shop</a></li>
-            <li><a href="cart.php" title="Cart">Cart</a></li>
-            <li><a href="profile.php" title="Account">Account</a></li>
-            <li><a href="logout.php" title="Logout">Logout</a></li>
-        </ul>
-    <?php else : ?>
-        <ul class="navbar">
-            <li><a href="shop.php" title="Shop">Shop</a></li>
-            <li><a href="login.php" title="Login">Login</a></li>
-        </ul>
-    <?php endif; ?>
+<?php endif; ?>
--- shop.php
+++ shop.php
@@ -2,32 +2,49 @@
 include 'koneksi.php';
 
 $sql = mysqli_query($konek, 'select * from product') or die(mysqli_error($konek));
-?><div class="product-container">
-    <?php
-    while ($data = mysqli_fetch_array($sql)) { ?>
-        <table border="1px solid black">
+if (isset($_SESSION['login'])) {
+    $btn_command = 'cart.php';
+} else {
+    $btn_command = 'login.php';
+}
+
+
+while ($data = mysqli_fetch_array($sql)) { ?>
+    <div class="product-container">
+        <!-- <table style="border:1px solid black;float:left;border-radius:12px;"> -->
+        <table>
             <tr>
-                <td colspan="2"><img width="360px" src="images/<?= $data['gambar'] ?>"></td>
+                <td colspan="2">
+                    <div class="img-wrap">
+                        <img class="product-img" title="<?= $data['product_name'] ?>" src="images/<?= $data['gambar'] ?>">
+                        <p class="img-description">Rp <?= $data['harga'] ?></p>
+                    </div>
+                </td>
             </tr>
 
             <div class="product-header">
                 <tr>
                     <td>
-                        <?php echo $data['product_name'] . "<br>"; ?>
+                        <!-- <?php echo $data['product_name'] . "<br>"; ?> -->
+                        <form method="POST" action="<?= $btn_command ?>">
+                            <input type="hidden" name="productid" value="<?= $data['product_id'] ?>">
+                            <input class="qty" type="text" name="quantity" value="1">
                     </td>
-                    <td rowspan="2">
-                        <?php if (isset($_SESSION['login'])) ?>
+                    <td>                       
+                            <input class="btn" style="width:150px;" type="submit" value="Add to Cart"></<input>
+                        </form>
                     </td>
                 </tr>
-                <tr>
+                <!-- <tr>
                     <td>
                         <?php echo "Rp " . $data['harga'] . "<br>"; ?>
                     </td>
 
-                </tr>
+                </tr> -->
             </div>
         </table>
-</div>
+    </div>
 <?php }
 
-include 'footer.php'; ?>
+//include 'footer.php'; 
+?>
