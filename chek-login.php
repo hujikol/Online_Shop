2d5183355cd2093e7bfdead1e055325ab65d1433
Nicholas Nanda <nikolasnanda@gmail.com>
2019-11-14 21:31:01 +0700

    Update chek-login.php

--- chek-login.php
+++ chek-login.php
@@ -8,19 +8,10 @@ $pass = md5($_POST['pass']);
 $data = mysqli_query($konek, "SELECT * FROM user where email= '$email' AND password = '$pass'");
 $row = mysqli_fetch_assoc($data);
 if (mysqli_num_rows($data) >= 1) {
-    
-    // contoh
-    // $data   = [
-    //     'firstname' => $firstname,
-    //     'login'     => true
-    // ];
-    // $_SESSION['data']['login'] =$data
-
-
     $_SESSION['nama'] = $row['firstname'];
     $_SESSION['uid'] = $row['user_id'];
     $_SESSION['login'] = true;
     header('Location:shop.php');
 } else {
-    header('Location:login.php?message=Login Gagal!');
+    header('Location:login.php?message=Failed to Login!');
 }
