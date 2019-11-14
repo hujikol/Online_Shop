8745371a151fd2fc036a93ad12618d0eeea9d682
Nicholas Nanda <nikolasnanda@gmail.com>
2019-11-14 21:31:14 +0700

    Update cart-con.php

--- cart-con.php
+++ cart-con.php
@@ -6,5 +6,35 @@ include 'koneksi.php';
 // variable sementara juga digunain nanti buat nampilin cart
 // jika user sudah memutuskan untuk bayar/ chekout baru dimasukkan ke database.
 
+$userid = $_SESSION['uid'];
+$qty = $_POST['quantity'];
+$productid = $_POST['productid'];
 
+$sql = mysqli_query($konek,"SELECT * FROM product WHERE product_id='$productid'");
+$data = mysqli_fetch_assoc($sql);
+
+// $newitem = array(
+//     $id = $productid
+//     'nm_produk' => 'hoodie', 
+//     'img_produk' => 'images/produk/hodie.jpg', 
+//     'harga_produk' => '20', 
+//     'qty' => '2' 
+//     );
+
+//     //jika tidak kosong
+//     if(!empty($_SESSION['cart']))
+//     {    
+//         //kalau produk sama
+//         if(isset($_SESSION['cart'][$id]) == $id) {
+//             $_SESSION['cart'][$id]['qty']++;
+//         } else { 
+//             //kalau beda buat baru
+//             $_SESSION['cart'][$id] = $newitem;
+//         }
+//     } else  {
+//         $_SESSION['cart'] = array();
+//         $_SESSION['cart'][$id] = $newitem;
+//     }
+
+    
 ?>
